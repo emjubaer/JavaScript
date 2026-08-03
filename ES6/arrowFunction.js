88ace1c55cca854b906f7ee2cefe918dce602feb
Jubaer <mdjubaer424@gmail.com>
2026-08-04 00:37:47 +0600

    Add arrow functions for greeting and squaring a number with example usage

---
+++ ES6/arrowFunction.js
@@ -0,0 +1,18 @@
+const greet = (name) =>{
+    return  `Hello! ${name}`
+}
+
+
+// Without Bracket for one parameter
+const greet2 = name =>{
+    return  `Hello! ${name}`
+}
+
+console.log(greet("Jubaer"));
+console.log(greet2("Jubaer"));
+
+// For one statement
+const square = number => number*number;
+
+console.log(square(5));
+
