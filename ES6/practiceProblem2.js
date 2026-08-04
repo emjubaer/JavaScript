3c0a025b195b41fe626bcf632ca8fb1c864c8b62
Jubaer <mdjubaer424@gmail.com>
2026-08-05 01:08:21 +0600

    Add product stock property, array manipulation examples, and object destructuring usage

--- ES6/practiceProblem2.js
+++ ES6/practiceProblem2.js
@@ -28,5 +28,59 @@ console.log(allKeys);
 const allDept = Object.values(employee);
 console.log(allDept);
 
+// copy product object add stock propery then show these objets
+
+const copyProduct = {...product, stock:10}
+console.log(product);
+console.log(copyProduct);
+
+const marks = [70,80,90];
+const newMarks = [...marks,100];
+console.log(Math.max(...newMarks));
+console.log(Math.min(...newMarks));
+
+const multiply = (a,b) => a*b;
+const square = (num) => num*num;
+
+console.log(multiply(2,4) ,square(5));
+
+
+// Defalut Parameter
+const greet = (name = "Guest") => `Welcome! ${name}`;
+console.log(greet());
+console.log(greet("Jubaer"));
+
+const colors = ["Red","Green","Blue","Yellow"];
+const [fColor, secColor,...othersColor] = colors;
+console.log(fColor, secColor);
+console.log(othersColor);
+console.log(...othersColor);
+
+// Object Destructuring 
+const user = {
+    id:1,
+    profile:{
+        name1:"John",
+        email:"john@gmail.com"
+    }
+}
+
+const {profile:{name1, email}} = user;
+console.log(name, email);
+
+
+const player = {
+    name2:"Messi",
+    team:"Inter Miami",
+    goals:[10,20,30]
+};
+
+// Destructure
+const {name2, team} = player;
+console.log(name2, team);
+
+// Spread goals copy
+const goalsCopy = {...player, goals:[...player.goals]};
+console.log(goalsCopy.goals);
 
 
